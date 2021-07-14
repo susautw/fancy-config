7a528abb8c660003be022380063f31763b4797c7
susautw <susautw@gmail.com>
2021-07-14 15:07:06 +0800

    update to 0.8.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with requirements_file.open() as fp:
 
 setup(
     name="fancy-config",
-    version="0.8.1",
+    version="0.8.2",
     packages=find_namespace_packages(),
     package_data={
         # If any package contains *.txt or *.rst files, include them:
