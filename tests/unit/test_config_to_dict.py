8d11d0d9367d09064a5225bd5ed38605291f342c
susautw <susautw@gmail.com>
2022-08-13 00:04:33 +0800

    Fix #18

---
+++ tests/unit/test_config_to_dict.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from fancy import config as cfg
+
+
+def test_config_to_dict_with_default_values():
+    class MySubConfig(cfg.BaseConfig):
+        x: bool = cfg.Option(default=True, type=bool)
+        y: Optional[float] = cfg.Option(nullable=True, type=float)
+
+    class MyConfig(cfg.BaseConfig):
+        a: int = cfg.Option(type=int)
+        b: float = cfg.Option(default=5.1, type=float)
+        sub: MySubConfig = cfg.Option(default={}, type=MySubConfig)
+
+    assert MyConfig(cfg.DictConfigLoader({'a': 1})).to_dict() == {
+        'a': 1,
+        'b': 5.1,
+        'sub': {'x': True, 'y': None}
+    }
