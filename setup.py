b58330c363dffb3ebb95dd44bc37c64e14785c60
susautw <susautw@gmail.com>
2025-01-29 00:17:11 +0800

    Version v0.13.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with requirements_file.open() as fp:
 
 setup(
     name="fancy-config",
-    version="0.13.0",
+    version="0.13.1",
     packages=find_namespace_packages(),
     package_data={
         # If any package contains *.txt or *.rst files, include them:
